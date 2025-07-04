797462a482327777663f3e1eeff0bce5a041cbdc
Kevin McLeod <kevinjhmcleod@gmail.com>
2025-07-04 11:19:58 -0400

    removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
 import { LazyMotion, domAnimation, motion } from 'framer-motion';
 
 export default function Home() {
